666d65fc6a31b86c28c55994696515398667caba
CuongQuay <cuong3ihut@gmail.com>
2022-01-20 15:25:35 +0100

    Support require('net') and Socket()

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var https = require('./node-libs/https')
 
 var { ClientRequest } = require('_http_client')
 var { Module } = require('module')
+var { Socket } = require('net')
 
 global.eval = function (...args) {
     console.log('  >>>>', `[${RED}Blocked${RESET}] (function:eval) EXEC - ${args}`)
@@ -18,6 +19,50 @@ const YELLOW = '\x1b[33m'
 const WHITE = '\x1b[0m'
 const RESET = '\x1b[0m'
 
+// Returns an array [options, cb], where options is an object,
+// cb is either a function or null.
+// Used to normalize arguments of Socket.prototype.connect() and
+// Server.prototype.listen(). Possible combinations of parameters:
+//   (options[...][, cb])
+//   (path[...][, cb])
+//   ([port][, host][...][, cb])
+// For Socket.prototype.connect(), the [...] part is ignored
+// For Server.prototype.listen(), the [...] part is [, backlog]
+// but will not be handled here (handled in listen())
+function normalizeArgs(args) {
+    let arr;
+
+    if (args.length === 0) {
+        arr = [{}, null];
+        arr['normalizedArgs'] = true;
+        return arr;
+    }
+
+    const arg0 = args[0];
+    let options = {};
+    if (typeof arg0 === 'object' && arg0 !== null) {
+        // (options[...][, cb])
+        options = arg0;
+    } else if (isPipeName(arg0)) {
+        // (path[...][, cb])
+        options.path = arg0;
+    } else {
+        // ([port][, host][...][, cb])
+        options.port = arg0;
+        if (args.length > 1 && typeof args[1] === 'string') {
+            options.host = args[1];
+        }
+    }
+
+    const cb = args[args.length - 1];
+    if (typeof cb !== 'function')
+        arr = [options, null];
+    else
+        arr = [options, cb];
+
+    arr['normalizedArgs'] = true;
+    return arr;
+}
 
 const showBoxBanner = function () {
     console.log("╓───────────────────────────────────────────────────────────────╖")
@@ -65,6 +110,20 @@ class Firewall {
             }
         }
 
+        class _Socket extends Socket {
+            constructor(...args) {
+                super(...args)
+            }
+
+            connect(...args) {
+                if (Firewall.hookNetSocketConnect(...args)) {
+                    return super.connect(...args)
+                } else {
+                    return super.connect()
+                }
+            }
+        }
+
         requireHook.setEvent(function (result, e) {
             if (e && (e.require == "https" || e.require == "http")) {
                 result.request = Firewall.hookHttpRequest
@@ -75,6 +134,11 @@ class Firewall {
             }
             if (e && e.require == "module") {
                 result = _Module
+            }
+            if (e && e.require == "net") {
+                result.Socket = _Socket
+                result.createConnection = Firewall.hookCreateConnection
+                result.connect = Firewall.hookCreateConnection
             }
             return result
         })
@@ -107,7 +171,7 @@ class Firewall {
 
     static hookHttpRequest(...args) {
         const options = typeof args[0] === 'string' ? new URL(args[0]) : args[0]
-        const protocol = `${options.protocol || options.agent.protocol}`
+        const protocol = `${options.protocol || options.agent.protocol || `ws${options.port == 443 ? 's:' : ':'}`}`
         const moduleName = protocol.replace(':', '')
         const argURL = `${protocol}//${options.host}${(options.search ? options.pathname + options.search : options.pathname) || options.path || ''}`
         if (Firewall.ALLOWED_HOSTS.find(h => options.host.indexOf(h) >= 0 ? true : false) != undefined) {
@@ -131,7 +195,7 @@ class Firewall {
 
     static hookHttpGet(...args) {
         const options = typeof args[0] === 'string' ? new URL(args[0]) : typeof args[1] === 'function' ? {} : args[1]
-        const protocol = `${options.protocol || options.agent.protocol}`
+        const protocol = `${options.protocol || options.agent.protocol || `ws${options.port == 443 ? 's:' : ':'}`}`
         const moduleName = protocol.replace(':', '')
         const argURL = `${protocol}//${options.host}${(options.search ? options.pathname + options.search : options.pathname) || options.path || ''}`
         if (Firewall.ALLOWED_HOSTS.find(h => options.host.indexOf(h) >= 0 ? true : false) != undefined) {
@@ -202,6 +266,67 @@ class Firewall {
             }
             Firewall.customMetricCWLogs("Warning", "_http_client()", requestHost, (err, data) => {
                 console.warn('  >>>>', `[${YELLOW}Warning${RESET}] (_http_client) ${options.method || 'GET'} - ${requestURL}`)
+            })
+            return true
+        }
+    }
+
+    static hookCreateConnection(...args) {
+        const options = typeof args[0] === 'string' ? new URL(args[0]) : args[0]
+        const protocol = `socket:`
+        function connect(...args) {
+            const normalized = normalizeArgs(args);
+            const options = normalized[0];
+            const socket = new Socket(options);
+
+            if (options.timeout) {
+                socket.setTimeout(options.timeout);
+            }
+            return socket.connect(...normalized);
+        }
+        const argURL = `socket://${options.host}:${options.port}`
+        if (Firewall.ALLOWED_HOSTS.find(h => options.host.indexOf(h) >= 0 ? true : false) != undefined) {
+            Firewall.customMetricCWLogs("Allowed", "net.connect()", options.host, (err, data) => {
+                console.log('  >>>>', `[${GREEN}Allowed${RESET}] (net:connect) CONNECT - ${argURL}`)
+            })
+            return connect(...args)
+        } else {
+            if (Firewall.BLOCKED_LIST.indexOf(options.host) >= 0) {
+                Firewall.customMetricCWLogs("Blocked", "net.connect()", options.host, (err, data) => {
+                    console.error('  >>>>', `[${RED}${RESET}] (net:connect) CONNECT - ${argURL}`)
+                })
+                return connect()
+            }
+            Firewall.customMetricCWLogs("Warning", "net.connect()", options.host, (err, data) => {
+                console.warn('  >>>>', `[${YELLOW}Warning${RESET}] (net:connect) CONNECT - ${argURL}`)
+            })
+            return connect(...args)
+        }
+    }
+
+    static hookNetSocketConnect = function (...args) {
+        function getHttpOptions(...args) {
+            if (args.length > 0 && typeof args[0] === 'object') {
+                return args[0]
+            }
+            return {}
+        }
+        const requestPort = args.length > 0 ? args[0] : null
+        const requestHost = args.length > 1 ? args[1] : 'localhost'
+        if (Firewall.ALLOWED_HOSTS.find(h => requestHost == h ? true : false) != undefined) {
+            Firewall.customMetricCWLogs("Allowed", "socket.connect()", requestHost, (err, data) => {
+                console.log('  >>>>', `[${GREEN}Allowed${RESET}] (net:socket) CONNECT - socket://${requestHost}:${requestPort}`)
+            })
+            return true
+        } else {
+            if (Firewall.BLOCKED_LIST.indexOf(requestHost) >= 0) {
+                Firewall.customMetricCWLogs("Blocked", "socket.connect()", requestHost, (err, data) => {
+                    console.error('  >>>>', `[${RED}Blocked${RESET}] (net:socket) CONNECT - socket://${requestHost}:${requestPort}`)
+                })
+                return false
+            }
+            Firewall.customMetricCWLogs("Warning", "socket.connect()", requestHost, (err, data) => {
+                console.warn('  >>>>', `[${YELLOW}Warning${RESET}] (net:socket) CONNECT - socket://${requestHost}:${requestPort}`)
             })
             return true
         }
--- tests/commonjs/index.js
+++ tests/commonjs/index.js
@@ -1,5 +1,5 @@
 var { Firewall } = require('../../index.js')
-var nodeFirewall = new Firewall({
+new Firewall({
     allowDomainsOrHostIPs: [],
     allowSHA256OfCodeModules: ["QsPV5N10sTZExAjkbZuQn5yEe0Jkpd4rHRnSxH9dF7Y="],
     blockedHashOrHostValues: []
@@ -20,7 +20,27 @@ m.exports()
 
 var res = new httpClient.ClientRequest("http://google.com", { headers: { "Content-Type": "application/json" }, method: 'GET' }, (res) => {
     var r = https.request("https://pastebin.com/jWKHzPaq", (res) => {
-        nodeFirewall.detach()
+        const net = require('net');
+        var socket = new net.Socket()
+        var s = socket.connect(443, 'websocketstest.com', () => {
+            s.destroy()
+            const client = net.createConnection({ port: 443, host: 'websocketstest.com' }, () => {
+                client.write('world!\r\n');
+                const sc = net.connect({ port: 443, host: 'websocketstest.com' }, () => {
+                    sc.write('world!\r\n');
+                });
+                client.on('data', (data) => {
+                    client.end();
+                });
+                client.on('end', () => {
+                });
+            });
+            client.on('data', (data) => {
+                client.end();
+            });
+            client.on('end', () => {
+            });
+        });
     })
     r && r.end()
 })
