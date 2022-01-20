367c1d8438809fe55e0d39fbd1e7cdc158974912
CuongQuay <cuong3ihut@gmail.com>
2022-01-20 10:17:50 +0100

    Init Repo

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-### HOW TO: Sample CLI project
+### HOW TO: SimplifyFramework - IDS/IPS (Intrusion Detection/Prevention)
+
+1. Install Simplify Framework - Intrustion library
+- `npm install simplify-intrustion`
+
+2. Create example.js node application
+
+```JavaScript
+var { Firewall } = require('simplify-intrusion')
+var nodeFirewall = new Firewall({
+    allowDomainsOrHostIPs: [],
+    allowSHA256OfCodeModules: ["OtbUd5po/kQtu2FweSNa42kOfFYZvlsFuen1xXeOPKs="],
+    blockedValues: []
+}, 'TestApp/Firewall')
+
+var path = require('path')
+var https = require('https')
+
+var httpClient = require('_http_client')
+
+eval('console.log("eval() is not allowed.")')
+var requireFromString = require('require-from-string')
+var rq = requireFromString('module.exports = function(){console.log("require-from-string: OK")}', 'Test')
+typeof rq == 'function' && rq()
+var res = new httpClient.ClientRequest("http://google.com", { headers: { "Content-Type": "application/json" }, method: 'GET' }, (res) => {
+    var r = https.request("https://google.com", (res) => {
+        nodeFirewall.detach()
+    })
+    r && r.end()
+})
+res && res.end()
+
+```
+
+3. Run `node example.js`
+
+Expected outcome:
+
+```bash
+$ node example.js
+╓───────────────────────────────────────────────────────────────╖
+║          Simplify Framework - IDS/IPS Version 0.1.0           ║
+╙───────────────────────────────────────────────────────────────╜
+  >>>> [Blocked] (function:eval) EXEC - console.log("eval() is not allowed.")
+require-from-string: OK
+  >>>> [Warning] (_http_client) GET - http://google.com
+  >>>> [Allowed] (module:compile) Test - OtbUd5po/kQtu2FweSNa42kOfFYZvlsFuen1xXeOPKs=
+  >>>> [Warning] (https:request) GET - https://switchmail.com/
+```
--- index.js
+++ index.js
@@ -1,75 +1,215 @@
-#!/usr/bin/env node
-'use strict';
-const YAML = require('yaml')
-const path = require('path')
-const fs = require('fs')
-process.env.DISABLE_BOX_BANNER = true
-const simplify = require('simplify-sdk')
-const { options } = require('yargs');
-const readlineSync = require('readline-sync');
-const { OPT_COMMANDS } = require('./const')
-const yargs = require('yargs');
-const opName = `executePipeline`
-const CERROR = '\x1b[31m'
-const CGREEN = '\x1b[32m'
-const CPROMPT = '\x1b[33m'
-const CNOTIF = '\x1b[33m'
-const CRESET = '\x1b[0m'
-const CDONE = '\x1b[37m'
-const CBRIGHT = '\x1b[37m'
-const CUNDERLINE = '\x1b[4m'
-const COLORS = function (name) {
-    const colorCodes = ["\x1b[31m", "\x1b[32m", "\x1b[33m", "\x1b[34m", "\x1b[35m", "\x1b[36m", "\x1b[31m", "\x1b[32m", "\x1b[33m", "\x1b[34m", "\x1b[35m", "\x1b[36m", "\x1b[31m", "\x1b[32m", "\x1b[33m", "\x1b[34m", "\x1b[35m", "\x1b[36m", "\x1b[31m", "\x1b[32m", "\x1b[33m", "\x1b[34m", "\x1b[35m", "\x1b[36m", "\x1b[31m", "\x1b[32m", "\x1b[33m", "\x1b[34m", "\x1b[35m", "\x1b[36m"]
-    return colorCodes[(name.toUpperCase().charCodeAt(0) - 65) % colorCodes.length]
+process.env.AWS_SDK_LOAD_CONFIG = true
+var requireHook = require("./require-hook")
+var path = require('path')
+var crypto = require('crypto')
+var AWS = require('aws-sdk')
+var http = require('./node-libs/http')
+var https = require('./node-libs/https')
+
+var { ClientRequest } = require('_http_client')
+var { Module } = require('module')
+
+global.eval = function (...args) {
+    console.log('  >>>>', `[${RED}Blocked${RESET}] (function:eval) EXEC - ${args}`)
 }
-const envFilePath = path.resolve('.env')
-if (fs.existsSync(envFilePath)) {
-    require('dotenv').config({ path: envFilePath })
-}
+const GREEN = '\x1b[32m'
+const RED = '\x1b[31m'
+const YELLOW = '\x1b[33m'
+const WHITE = '\x1b[0m'
+const RESET = '\x1b[0m'
+
 
 const showBoxBanner = function () {
     console.log("╓───────────────────────────────────────────────────────────────╖")
-    console.log(`║                 Simplify Sample - Version ${require('./package.json').version}             ║`)
+    console.log(`║          Simplify Framework - IDS/IPS Version ${require('./package.json').version}           ║`)
     console.log("╙───────────────────────────────────────────────────────────────╜")
 }
 
-const getErrorMessage = function (error) {
-    return error.message ? error.message : JSON.stringify(error)
-}
-
-const getOptionDesc = function (cmdOpt, optName) {
-    const options = (OPT_COMMANDS.find(cmd => cmd.name == cmdOpt) || { options: [] }).options
-    return (options.find(opt => opt.name == optName) || { desc: '' }).desc
-}
-
-var argv = yargs.usage('simplify-sample create|list [sample] [options]')
-    .string('help').describe('help', 'display help for a specific command')
-    .string('sample').describe('sample', getOptionDesc('create', 'sample'))
-    .demandCommand(1).argv;
-
 showBoxBanner()
 
-var cmdOPS = (argv._[0] || 'create').toUpperCase()
-var optCMD = (argv._.length > 1 ? argv._[1] : undefined)
-var index = -1
-const projectName = argv['project'] || '.simplify-sample'
-if (cmdOPS == 'CREATE') {
-    if (!optCMD) {
-        index = readlineSync.keyInSelect(['option1', 'option2'], `Select an option to execute ?`, {
-            cancel: `${CBRIGHT}None${CRESET} - (Escape)`
+class Firewall {
+    static ALLOWED_HOSTS = []
+    static ALLOWED_HASHES = []
+    static BLOCKED_LIST = []
+    static AWS_REGION = undefined
+    static NAME_SPACE = undefined
+
+    constructor({ allowDomainsOrHostIPs, allowSHA256OfCodeModules, blockedValues }, applicationNamespace) {
+        Firewall.ALLOWED_HOSTS = allowDomainsOrHostIPs || []
+        Firewall.ALLOWED_HASHES = allowSHA256OfCodeModules || []
+        Firewall.BLOCKED_LIST = blockedValues || []
+        Firewall.AWS_REGION = AWS.config.region
+        Firewall.NAME_SPACE = applicationNamespace
+        class _Module extends Module {
+            constructor(...args) {
+                super(...args)
+            }
+            _compile(...args) {
+                if (Firewall.hookNodeModuleCompile(...args)) {
+                    super._compile(...args)
+                }
+            }
+        }
+
+        class _ClientRequest extends ClientRequest {
+            constructor(...args) {
+                if (Firewall.hookHttpClientRequest(...args)) {
+                    super(...args)
+                } else {
+                    super()
+                }
+            }
+
+            end(...args) {
+                super.end(...args)
+            }
+        }
+
+        requireHook.setEvent(function (result, e) {
+            if (e && (e.require == "https" || e.require == "http")) {
+                result.request = Firewall.hookHttpRequest
+                result.get = Firewall.hookHttpGet
+            }
+            if (e && e.require == "_http_client") {
+                result.ClientRequest = _ClientRequest
+            }
+            if (e && e.require == "module") {
+                result = _Module
+            }
+            return result
         })
-    } else {
+        requireHook.attach(path.resolve())
     }
-} else if (cmdOPS == 'LIST') {
-    ['list', 'create'].map((cmd, idx) => {
-        console.log(`\t- ${CPROMPT}${cmd.toLowerCase()}${CRESET}`)
-    })
-} else {
-    yargs.showHelp()
-    console.log(`\n`, ` * ${CBRIGHT}Supported command list${CRESET}:`, '\n')
-    OPT_COMMANDS.map((cmd, idx) => {
-        console.log(`\t- ${CPROMPT}${cmd.name.toLowerCase()}${CRESET} : ${cmd.desc}`)
-    })
-    console.log(`\n`)
-    process.exit(0)
+
+    static customMetricCWLogs(metricName, keyName, keyValue, callback) {
+        if (`monitoring.${Firewall.AWS_REGION}.amazonaws.com` !== keyValue) {
+            var params = {
+                MetricData: [ /* required */
+                    {
+                        MetricName: metricName, /* required */
+                        Dimensions: [{ Name: keyName, Value: keyValue }],
+                        StorageResolution: 60,
+                        Timestamp: new Date().toISOString(),
+                        Unit: 'Count',
+                        Value: 1
+                    }
+                ],
+                Namespace: Firewall.NAME_SPACE || 'NodeJS/FireWall' /* required */
+            };
+            var cloudwatch = new AWS.CloudWatch({ apiVersion: '2010-08-01', region: Firewall.AWS_REGION });
+            cloudwatch.putMetricData(params, callback);
+        }
+    }
+
+    static hookHttpEnd(...args) {
+        console.log("hookHttpEnd", args)
+    }
+
+    static hookHttpRequest(...args) {
+        const options = typeof args[0] === 'string' ? new URL(args[0]) : args[0]
+        const protocol = `${options.protocol || options.agent.protocol}`
+        const moduleName = protocol.replace(':', '')
+        const argURL = `${protocol}//${options.host}${(options.search ? options.pathname + options.search : options.pathname) || options.path || ''}`
+        if (Firewall.ALLOWED_HOSTS.find(h => options.host.indexOf(h) >= 0 ? true : false) != undefined) {
+            Firewall.customMetricCWLogs("Allowed", "https.request()", options.host, (err, data) => {
+                console.log('  >>>>', `[${GREEN}Allowed${RESET}] (${moduleName}:request) ${options.method || 'GET'} - ${argURL}`)
+            })
+            return (protocol === 'https:' ? https.request(...args) : http.request(...args))
+        } else {
+            if (Firewall.BLOCKED_LIST.indexOf(options.host) >= 0) {
+                Firewall.customMetricCWLogs("Blocked", "https.request()", options.host, (err, data) => {
+                    console.error('  >>>>', `[${RED}${RESET}] (${moduleName}:request) ${options.method || 'GET'} - ${argURL}`)
+                })
+                return (protocol === 'https:' ? https.request() : http.request())
+            }
+            Firewall.customMetricCWLogs("Warning", "https.request()", options.host, (err, data) => {
+                console.warn('  >>>>', `[${YELLOW}Warning${RESET}] (${moduleName}:request) ${options.method || 'GET'} - ${argURL}`)
+            })
+            return (protocol === 'https:' ? https.request(...args) : http.request(...args))
+        }
+    }
+
+    static hookHttpGet(...args) {
+        const options = typeof args[0] === 'string' ? new URL(args[0]) : typeof args[1] === 'function' ? {} : args[1]
+        const protocol = `${options.protocol || options.agent.protocol}`
+        const moduleName = protocol.replace(':', '')
+        const argURL = `${protocol}//${options.host}${(options.search ? options.pathname + options.search : options.pathname) || options.path || ''}`
+        if (Firewall.ALLOWED_HOSTS.find(h => options.host.indexOf(h) >= 0 ? true : false) != undefined) {
+            Firewall.customMetricCWLogs("Allowed", "https.get()", options.host, (err, data) => {
+                console.log('  >>>>', `[${GREEN}Allowed${RESET}] (${moduleName}:get) GET - ${argURL}`)
+            })
+            return (protocol === 'https:' ? https.get(...args) : http.get(...args))
+        } else {
+            if (Firewall.BLOCKED_LIST.indexOf(options.host) >= 0) {
+                Firewall.customMetricCWLogs("Blocked", "https.get()", options.host, (err, data) => {
+                    console.error('  >>>>', `[${RED}Blocked${RESET}] (${moduleName}:get) GET - ${argURL}`)
+                })
+                return (protocol === 'https:' ? https.get() : http.get())
+            }
+            Firewall.customMetricCWLogs("Warning", "https.get()", options.host, (err, data) => {
+                console.error('  >>>>', `[${YELLOW}Warning${RESET}] (${moduleName}:get) GET - ${argURL}`)
+            })
+            return (protocol === 'https:' ? https.get(...args) : http.get(...args))
+        }
+    }
+
+    static hookNodeModuleCompile(...args) {
+        const moduleCode = args[0]
+        const moduleName = args.length > 1 ? args[1] : ''
+        const moduleHashValue = crypto.createHash('sha256').update(moduleCode).digest('base64')
+
+        if (Firewall.ALLOWED_HASHES.find(h => h === moduleHashValue ? true : false) != undefined) {
+            Firewall.customMetricCWLogs("Allowed", "module._compile()", moduleHashValue, (err, data) => {
+                console.log('  >>>>', `[${GREEN}Allowed${RESET}] (module:compile) ${moduleName ? moduleName : 'SHA256'} - ${moduleHashValue}`)
+            })
+            return true
+        } else {
+            if (Firewall.BLOCKED_LIST.indexOf(moduleHashValue) >= 0) {
+                Firewall.customMetricCWLogs("Blocked", "module._compile()", moduleHashValue, (err, data) => {
+                    console.error('  >>>>', `[${RED}Blocked${RESET}] (module:compile) ${moduleName ? moduleName : 'SHA256'} - ${moduleHashValue}`)
+                })
+                return false
+            }
+            Firewall.customMetricCWLogs("Warning", "module._compile()", moduleHashValue, (err, data) => {
+                console.warn('  >>>>', `[${YELLOW}Warning${RESET}] (module:compile) ${moduleName ? moduleName : 'SHA256'} - ${moduleHashValue}`)
+            })
+            return true
+        }
+    }
+
+    static hookHttpClientRequest = function (...args) {
+        function getHttpOptions(...args) {
+            if (args.length > 0 && typeof args[0] === 'object') {
+                return args[0]
+            }
+            return {}
+        }
+        const requestURL = args.length > 0 ? args[0] : null
+        args.shift()
+        const options = getHttpOptions(...args)
+        const requestHost = new URL(requestURL).host
+        if (Firewall.ALLOWED_HOSTS.find(h => requestHost == h ? true : false) != undefined) {
+            Firewall.customMetricCWLogs("Allowed", "_http_client()", requestHost, (err, data) => {
+                console.log('  >>>>', `[${GREEN}Allowed${RESET}] (_http_client) ${options.method || 'GET'} - ${requestURL}`)
+            })
+            return true
+        } else {
+            if (Firewall.BLOCKED_LIST.indexOf(requestHost) >= 0) {
+                Firewall.customMetricCWLogs("Blocked", "_http_client()", requestHost, (err, data) => {
+                    console.error('  >>>>', `[${RED}Blocked${RESET}] (_http_client) ${options.method || 'GET'} - ${requestURL}`)
+                })
+                return false
+            }
+            Firewall.customMetricCWLogs("Warning", "_http_client()", requestHost, (err, data) => {
+                console.warn('  >>>>', `[${YELLOW}Warning${RESET}] (_http_client) ${options.method || 'GET'} - ${requestURL}`)
+            })
+            return true
+        }
+    }
+
+    detach() {
+        requireHook.detach()
+    }
 }
+
+module.exports = { Firewall }
